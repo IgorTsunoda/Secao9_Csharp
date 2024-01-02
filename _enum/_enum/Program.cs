4c0219917241707b73f7354edefb474116c5fcd5
Igor Tsunoda <igortaikitsunoda@gmail.com>
2024-01-02 10:35:10 -0300

    commit teste

---
+++ _enum/_enum/Program.cs
@@ -0,0 +1,27 @@
+﻿using _enum.Entities;
+using _enum.Entities.Enums;
+
+namespace _enum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Order order = new Order
+            {
+                Id = 1080,
+                Moment = DateTime.Now,
+                Status = OrderStatus.PendingPayment
+            };
+
+            Console.WriteLine(order);
+
+            string txt = OrderStatus.PendingPayment.ToString();
+            OrderStatus os = Enum.Parse<OrderStatus>("Delivered");
+
+            Console.WriteLine(os);
+
+
+        }
+    }
+}
--- enum/enum/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace internal enum;
-
-internal class Program
-{
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello, World!");
-    }
-}
